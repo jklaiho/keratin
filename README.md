f9585e9c535ce18f163a50c95608687fb4181aea
JK Laiho <jklaiho@iki.fi>
2012-01-08 14:15:55 +0200

    Boilerplate

---
+++ README.md
@@ -0,0 +1,8 @@
+Keratin
+=======
+
+Keratin is a static site generator that tries very hard not to suck.
+
+Not sucking is a goal for 1.0.
+
+What you're seeing here is not 1.0.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+from keratin import VERSION
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(
+    name='Keratin',
+    version=".".join(map(str, VERSION)),
+    description='Keratin is a static site generator that tries very hard not '
+        'to suck.'
+    long_description=readme,
+    author='JK Laiho',
+    author_email='jklaiho@iki.fi',
+    url='https://github.com/jklaiho/keratin',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ],
+    test_suite='keratin.tests'
+)
